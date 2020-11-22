691e7ee7c80cc9dc59a33dd5cb189854e9b7995e
annamde <annadiaz553@gmail.com>
2020-11-22 15:07:48 +0100

    Modo 1 placeholder + drag objects

---
+++ RevistaProject/Assets/_Scripts/Mode1ChakrasScirpt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mode1ChakrasScirpt : MonoBehaviour
+{
+    public int speed = 3;
+    public SpriteRenderer mySprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -1 * (Camera.main.transform.position.z));
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+    }
+}
