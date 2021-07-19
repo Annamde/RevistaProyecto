64e3809ab924150a1c115898aa1ffc50d7ace219
annamde <annadiaz553@gmail.com>
2021-07-19 20:11:50 +0200

    add chakra and barrita no wrong type image

--- RevistaProject/Assets/_Scripts/HuecoScript.cs
+++ RevistaProject/Assets/_Scripts/HuecoScript.cs
@@ -13,10 +13,21 @@ public class HuecoScript : MonoBehaviour
 
     public Image filler;
     public Image completedImage; //imagen con el chakra o la barra
+
+    [SerializeField]
+    private SpriteRenderer _spriteRenderer;
+
+    [SerializeField]
+    private Sprite _spriteCorrectType;
     
     void Start()
     {
         isCompleted = false;
+
+        if (this.tag != "TypeWrong")
+        {
+            _spriteRenderer.sprite = _spriteCorrectType;
+        }
     }
     
     void Update()
