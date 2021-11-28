c37f303fd6db4ed53b8a754d2dbf1e48da7d5186
annamde <annadiaz553@gmail.com>
2021-11-28 17:46:22 +0100

    botones en el arbol para abrir el canvas que toca

---
+++ RevistaProject/Assets/_Scripts/ButtonChakraTreeController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonChakraTreeController : MonoBehaviour
+{
+    [SerializeField] private Image _imageContainer;
+    [SerializeField] private Sprite _chakraInfoImage;
+
+    public void SetUpInfoChakra()
+    {
+        _imageContainer.sprite = _chakraInfoImage;
+        _imageContainer.gameObject.SetActive(true);
+    }
+
+    public void CloseInfoChakra()
+    {
+        _imageContainer.gameObject.SetActive(false);
+    }
+}
